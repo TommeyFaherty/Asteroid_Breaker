d1a7c2ba0aad34c629844039c3260dca650b8645
TommeyFaherty <tommeyf3@hotmail.com>
2019-10-20 00:40:19 +0100

    Addition to sound
    Added temporary music for in game and ability to mute it and change the volume

---
+++ Asteriod Breaker/Asteriod Breaker/Assets/__Scripts/Dock.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dock : MonoBehaviour
+{
+    [SerializeField]
+    private float Height = 0.5f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position,
+                            new Vector3(Height, 0.5f));
+    }
+}
---
+++ Asteriod Breaker/Asteriod Breaker/Assets/__Scripts/PointSpawner.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utilities;
+
+public class PointSpawner : MonoBehaviour
+{
+    // get a list of spawn points
+    // randomly select one to get the position at which to start the enemy
+    [SerializeField]
+    private float spawnDelay = 1.0f;
+
+    [SerializeField]
+    private float spawnInterval = 0.5f;
+
+    [SerializeField]
+    private Enemy enemyPrefab;
+
+    private GameObject enemyParent;
+
+    private IList<SpawnPoint> spawnPoints;
+
+    private Stack<SpawnPoint> spawnStack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get the enemy parent
+        enemyParent = ParentUtils.GetEnemyParent();
+        spawnPoints = GetComponentsInChildren<SpawnPoint>();
+        SpawnRepeating();
+    }
+
+    private void SpawnRepeating()
+    {
+        // create a shuffled stack
+        spawnStack = ListUtils.CreateShuffledStack(spawnPoints);
+        InvokeRepeating("Spawn", spawnDelay, spawnInterval);
+    }
+
+    private void Spawn()
+    {
+        //var randomIndex = UnityEngine.Random.Range(0, spawnPoints.Count);
+        //var currPoint = spawnPoints[randomIndex];
+        // get the next item off the stack - pop
+        // if stack is empty, reshuffle
+        if( spawnStack.Count == 0)
+        {
+            spawnStack = ListUtils.CreateShuffledStack(spawnPoints);
+        }
+        var currPoint = spawnStack.Pop();
+        var enemy = Instantiate(enemyPrefab, enemyParent.transform);
+        enemy.transform.position = currPoint.transform.position;
+    }
+
+}
